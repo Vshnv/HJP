3537ccbe844711ce7dad6fba87b5b8cc732f5d8c
anfaas1618 <ishad0911@gmail.com>
2019-10-22 22:06:54 +0530

    insert

--- src/project/codewick/Codewick.java
+++ src/project/codewick/Codewick.java
@@ -1,7 +1,13 @@
 package project.codewick;
 
 public class Codewick {
-    public static void main(String[] args){
+    public  static void insert()
+    {
 
     }
+
+    public static void main(String[] args){
+    insert();
+    }
+
 }
--- src/project/codewick/utils/SQL.java
+++ src/project/codewick/utils/SQL.java
@@ -1,6 +1,6 @@
 package project.codewick.utils;
 
-import java.sql.Connection;
+        import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
